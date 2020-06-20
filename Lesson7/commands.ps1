39281a4a7a5821ecb759b7ebab962902ae000280
Pawel Wilkosz <pawelwilkosz@Pawels-MacBook-Air.local>
2020-06-20 07:42:09 +0200

    Homework

--- Lesson7/commands.ps1
+++ Lesson7/commands.ps1
@@ -29,3 +29,13 @@ Wait-Job
 # Job scheduler
 Register-ScheduledJob -Name DailyProc -ScriptBlock { Get-Process } -Trigger (New-JobTrigger -Daily -At 2am) -ScheduledJobOption (New-ScheduledJobOption -WakeToRun-RunElevated)
 
+### Homework
+# Create job which will find all bash scripts
+Start-Job { dir / -recurse -filter '*.sh' }
+Receive-Job -Id <job_id>
+
+# Create job above on remote computers
+Invoke-Command -scriptblock { ls / -recurse -filter *.sh } -computerName 127.0.0.1,localhost -asjob
+
+# getting from txt file
+Invoke-Command -scriptblock { ls / -recurse -filter *.sh } -computerName (get-content computers.txt) -asjob
