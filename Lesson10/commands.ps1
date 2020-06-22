5ab4fb78e4b57cc56719410416fcca9ed5d1401e
Pawel Wilkosz-PQM648 <pawel.wilkosz@motorolasolutions.com>
2020-06-22 18:09:43 +0200

    Lesson 10 source code

---
+++ Lesson10/commands.ps1
@@ -0,0 +1,18 @@
+# Warning
+Write-Warnig "Compilation Error"
+
+# Debug
+$DebugPreference="Continue"
+Write-Debug "Endpoint shall be there"
+
+# Verbose
+$VerbosePreference="Continue"
+Write-Verbose "Verbose Test"
+
+# Error
+Write-Error "Failure happen here"
+
+
+
+
+
